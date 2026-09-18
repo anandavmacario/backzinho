d2aa959cfd6a93ecf6ab95a184cc632c5f396a8c
anandavmacario <nanda10vm@gmail.com>
2026-09-18 11:04:14 -0300

    1

---
+++ aula2/conceito.js
@@ -0,0 +1,15 @@
+// Início backend - JAVASCRIPT
+
+//Variáveis
+
+//ANTIGO, pode ser redeclarado e mudar o valor.
+var idade = 10
+var idade = 20 // não gera erro
+
+//Mais moderno, pode mudar de valor, mas não pode ser redeclarado
+let nome = "Yuki"
+nome = "Yukao"; //permitido
+
+//const é fixo, não pode mudar de valor
+const pi = 3.14;
+//pi = 40; //
---
+++ aula2/exercicio_01.js
@@ -0,0 +1,3 @@
+//Atividade Number
+//Crie uma variável chama anoAtual e outra chamada anoNascimento.
+//Calcule e exiba a idade no console.
