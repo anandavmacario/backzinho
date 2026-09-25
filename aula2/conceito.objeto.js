162c91128e56bc48fafe233c071ae45306064d00
anandavmacario <nanda10vm@gmail.com>
2026-09-25 09:34:05 -0300

    ei

---
+++ aula2/conceito.objeto.js
@@ -0,0 +1,22 @@
+//Object (objeto).
+//Um objeto agrupa várias informações dentro de uma mesma variável, usando chave e valor.
+
+let pessoa = {
+nome: "Geto",
+idade: 17,
+cidade: "Tokyo"
+};
+
+console.log(pessoa)
+
+//Crie um objeto chamado 'carro' com as propriedades 'marca', 'modelo' e 'ano'.
+//Exiba cada propriedade no console.
+let carro = {
+    marca: "Ford",
+    modelo: "Ecosport",
+    ano: 2006
+}
+
+console.log(carro.marca)
+console.log(carro.modelo)
+console.log(carro.ano)
---
+++ aula2/conceito_array.js
@@ -0,0 +1,18 @@
+//Array.
+//Um array é uma lista que pode armazenar vários valores.
+
+let frutas = ["Maçã", "Banana", "uva"];
+// let frutas = ["0", "1", "2"]
+console.log(frutas[1])
+
+//Podemos adicionar itens! Sem alterar o array diretamente
+frutas.push("laranja"); //Adiciona ao final
+console.log(frutas[3]);
+frutas.pop();
+console.log(frutas);
+
+//Crie um Array chamado 'animais' e adicione três animais.
+//Exiba o primeiro e o último console.
+let animais = ["Raposa", "Furão", "Tigre"];
+console.log(animais[0])
+console.log(animais[2])
